2a78aff1487ed8f04b7f045ba2a63e80da1bfb69
DonAmit197 <amitabhaghosh197@hotmail.com>
2024-01-17 11:49:44 +1300

    Added Temp NZBN lookup

--- modules/createForm.js
+++ modules/createForm.js
@@ -1,6 +1,7 @@
 //import gds from '@ukhomeoffice/formio-gds-template';
 import { formSubmitEvent, submissionConfirmation } from './form-submit-event';
 import { confirmationUIInit, tipsUI } from './confirmation-ui';
+import nzbnNumber from './nzbnNumber';
 import footer from './footer';
 const _ = require('lodash');
 
@@ -15,10 +16,12 @@ export const createForm = (formioForm, result) => {
     },
   }).then((form) => {
     console.log(form);
+    nzbnNumber(form);
     formSubmitEvent(form);
     submissionConfirmation(form);
     confirmationUIInit(form);
     tipsUI(form);
+
     footer(form);
   });
 };
---
+++ modules/nzbnNumber.js
@@ -0,0 +1,19 @@
+export default function nzbnNumber(form) {
+  form.everyComponent((component) => {
+    if (component.component.key === 'data_entity_nzbn1') {
+      console.log('NZBN', component);
+      form.ready.then(function () {
+        //const nzbnComp = component.component;
+        if (nzbnComp.data.url === 'data_nzbnentityurl') {
+          nzbnComp.data.url = 'https://api.business.govt.nz/sandbox/nzbn/v5/entities';
+        }
+        //const authKey = nzbnComp.data.headers;
+        console.log(nzbnComp.data.headers[0].value);
+        if (nzbnComp.data.headers[0].value === 'data_nzbnbasesubscriptionkey') {
+          nzbnComp.data.headers[0].value = '22c5825ca2bd40cba2b30e684df323cc';
+        }
+        component.redraw();
+      });
+    }
+  });
+}
